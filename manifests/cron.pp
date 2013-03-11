0d693a87b7dda4c050cc2dc5f633f920b7bad51d
Simon Josi <me@yokto.net>
2013-03-11 13:38:36 +0100

    puppet::cron: use cron::crond and escape percentage sign

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -4,10 +4,9 @@ class puppet::cron inherits puppet::service {
     ensure => stopped,
   }
 
-  cron { 'puppet':
-    command => "sleep $((RANDOM%59)) && /usr/sbin/puppet agent --config ${puppet::dir}/puppet.conf --onetime --no-daemonize",
+  cron::crond{'puppet':
+    command => "sleep $((RANDOM\%59)) && /usr/sbin/puppet agent --config ${puppet::dir}/puppet.conf --onetime --no-daemonize",
     user    => root,
     minute  => ip_to_cron($puppet::cron_interval, $puppet::cron_range),
   }
-
 }
